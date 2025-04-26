80e74839877b7acfd6f95f9af2ff56accebf0a01
ludohome <ludovic@coquemert.fr>
2025-04-26 10:15:01 +0200

    Configuration et Connexion à la base de données

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Déclaration des constantes pour la connection à la base de données, DB_NAME, DB_HOST, DB_USER et DB_PASSWORD sont à modifier.
+define('DB_NAME', 'carnetadresse');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
